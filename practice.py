eb28149bcfa4a2c935f0104ae519d7514e646cd7
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 22:19:52 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,4 +1,11 @@
-print('hello')
-print('world')
-print('world')
-print()
+>>> s = 'pining for the fjords'
+>>> t = s.split()
+>>> t
+
+['pining', 'for', 'the', 'fjords']
+
+>>> s = 'spam-spam-spam'
+>>> delimiter = '-'
+>>> t s.split(delimiter)
+>>> t
+['spam', 'spam', 'spam']
